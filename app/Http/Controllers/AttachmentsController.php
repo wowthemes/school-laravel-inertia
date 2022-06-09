7ede590d1253472335b3f86cbc9f816f0c717e6e
wowthemes <shehbaz2009@gmail.com>
2022-06-09 05:30:32 +0500

    Working on attachments

--- app/Http/Controllers/AttachmentsController.php
+++ app/Http/Controllers/AttachmentsController.php
@@ -2,14 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Attachment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class AttachmentsController extends Controller
 {
     public function store(Request $request)
     {
         $files = $request->file('file');
+        $response = [];
 
-        dd($files);
+        foreach ($files as $file) {
+            $orignal_name = $file->getClientOriginalName();
+            $path = date('Y') . '/' . date('m');
+            
+            $res = $file->storeAs($path, $orignal_name);
+            $storage = Storage::get($res);
+            $model = new Attachment();
+            $model->original_name = $orignal_name;
+            $model->name = $orignal_name;
+            $model->mime = Storage::mimeType($res);
+            $model->extension = $file->getClientOriginalExtension();
+            $model->size = Storage::size($res);
+            $model->path = $res;
+            $model->disk = config('filesystems.default');
+            $model->user_id = auth()->id();
+            $model->save();
+
+            $response[] = $model;
+
+        }
+
+        return response()->json($response);
     }
 }
--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DISK', 'local'),
+    'default' => env('FILESYSTEM_DISK', 'uploads'),
 
     /*
     |--------------------------------------------------------------------------
@@ -33,6 +33,14 @@ return [
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
+            'throw' => false,
+        ],
+
+        'uploads' => [
+            'driver' => 'local',
+            'root' => public_path('uploads'),
+            'url' => env('APP_URL').'/uploads',
+            'visibility' => 'public',
             'throw' => false,
         ],
 
